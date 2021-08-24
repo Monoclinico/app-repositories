3030ccbdb2a50461621f3c446d1dabce2e1ee24c
Matheus Sanches <matheussanchesleme@gmail.com>
2021-08-24 19:38:28 -0300

    Início do projeto

---
+++ README.md
@@ -0,0 +1,14 @@
+# App para Apresentar o Portfólio do GitHub
+
+
+
+## Descrição do Aplicativo.
+> App Android para apresentar portfólios de projetos do GitHub de maneira elegante e simplificada. O projeto foi desenvolvido usando o Kotlin, uma 
+das linguagens de programação de maior ascensão nos últimos anos.
+
+## Sobre o Autor
+<img align="left" width="80" height="80" margin-right="150px" style="border-radius: 10%; margin: 10px" src="https://avatars.githubusercontent.com/u/35543003?v=4">
+
+Gosto de usar linguagens de programação como ferramentas para solucionar os mais diversos problemas. Estou sempre procurando aprender coisas novas para me ajudarem a sempre desempenhar o meu máximo.
+
+[Linkedin](https://www.linkedin.com/in/matheus-sanches-55336b60/) [Github](https://github.com/Monoclinico) [E-mail](mailto:matheussanchesleme@outlook.com)
---
+++ app/src/main/java/br/com/dio/app/repositories/ui/MainActivity.kt
@@ -0,0 +1,70 @@
+package br.com.dio.app.repositories.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import br.com.dio.app.repositories.R
+import br.com.dio.app.repositories.core.createDialog
+import br.com.dio.app.repositories.core.createProgressDialog
+import br.com.dio.app.repositories.core.hideSoftKeyboard
+import br.com.dio.app.repositories.databinding.ActivityMainBinding
+import br.com.dio.app.repositories.presentation.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    private val dialog by lazy { createProgressDialog() }
+    private val viewModel by viewModel<MainViewModel>()
+    private val adapter by lazy { RepoListAdapter() }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root) //colocando o interface visual da atividade
+        setSupportActionBar(binding.toolbar) //colocando a toolbar do layout da atividade
+        binding.rvRepos.adapter = adapter
+
+        viewModel.repos.observe(this) {
+            when (it) {
+                MainViewModel.State.Loading -> dialog.show()
+                is MainViewModel.State.Error -> {
+                    createDialog {
+                        setMessage(it.error.message)
+                    }.show()
+                    dialog.dismiss()
+                }
+                is MainViewModel.State.Success -> {
+                    dialog.dismiss()
+                    adapter.submitList(it.list)
+                }
+            }
+        }
+    }
+
+    //menu da atividade
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        query?.let { viewModel.getRepoList(it) }
+        binding.root.hideSoftKeyboard()
+        return true
+    }
+
+    //método é chamdo quando o texto da pesquisa é mudado
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.e(TAG, "onQueryTextChange: $newText")
+        return false
+    }
+
+    companion object {
+        private const val TAG = "TAG"
+    }
+
+}
